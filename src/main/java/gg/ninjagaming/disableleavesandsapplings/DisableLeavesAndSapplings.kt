69981195291258bad196ef7eb391986a014db172
Luca <Luca@Taschner.me>
2023-12-20 10:42:15 +0100

    Added event registration to onEnable function

--- src/main/java/gg/ninjagaming/disableleavesandsapplings/DisableLeavesAndSapplings.kt
+++ src/main/java/gg/ninjagaming/disableleavesandsapplings/DisableLeavesAndSapplings.kt
@@ -1,10 +1,19 @@
 package gg.ninjagaming.disableleavesandsapplings
 
+import gg.ninjagaming.disableleavesandsapplings.eventlisteners.BlockGrowEventListener
+import gg.ninjagaming.disableleavesandsapplings.eventlisteners.LeavesDecayEventListener
 import org.bukkit.plugin.java.JavaPlugin
 
 class DisableLeavesAndSapplings : JavaPlugin() {
     override fun onEnable() {
         // Plugin startup logic
+        registerEvents()
+
+    }
+
+    private fun registerEvents() {
+        server.pluginManager.registerEvents(LeavesDecayEventListener(), this)
+        server.pluginManager.registerEvents(BlockGrowEventListener(), this)
     }
 
     override fun onDisable() {
